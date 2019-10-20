27f653ab2708539dc9038b19f472ed307f22ff3f
郭俊廷 <>
2019-10-20 14:37:17 +0800

    finished AddTwoNumber better than 32.67%

--- Algorithms/AddTwoNumbers.py
+++ Algorithms/AddTwoNumbers.py
@@ -5,28 +5,46 @@ class ListNode(object):
         self.next = None
 
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        x = self.getValue(l1)
-        print(x.val)
-    
-    def getValue(self, l):
-        if l.next != None:
-            self.getValue(l.next)
-        else:
-            return l
+    def addTwoNumbers(self, l1:ListNode, l2:ListNode) -> ListNode :
+        curr1 = l1
+        curr2 = l2
+        carry = 0
+        total = None
+        current = None
         
+        
+        while curr1 != None or curr2 != None or carry != 0:
+            sum = 0
+            sum += carry if carry != 0 else 0
+            sum += curr1.val if curr1 != None else 0
+            sum += curr2.val if curr2 != None else 0
+            
+            if sum >= 10:
+                sum -= 10
+                carry = 1
+            else:
+                carry = 0
+            
+            if total == None:
+                total = ListNode(sum)
+                current = total
+            else:
+                current.next = ListNode(sum)
+                current = current.next
+                
+            curr1 = curr1.next if curr1 != None else None
+            curr2 = curr2.next if curr2 != None else None
+         
+        return total   
+    
+    
 l1 = ListNode(2)
 l1.next = ListNode(4)
 l1.next.next = ListNode(3)
 
-l1 = ListNode(5)
-l1.next = ListNode(6)
-l1.next.next = ListNode(4)
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
 
 s = Solution()
-s.addTwoNumbers(l1,l1)
+total = s.addTwoNumbers(l1,l2)
