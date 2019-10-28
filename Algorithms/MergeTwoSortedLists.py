950a2f0861bfdab8da094be4db8d4800a662f643
郭俊廷 <>
2019-10-29 00:42:45 +0800

    developing quick sort algo.

--- Algorithms/MergeTwoSortedLists.py
+++ Algorithms/MergeTwoSortedLists.py
@@ -5,12 +5,56 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
         last = l1
         while last.next != None:
             last = last.next
         last.next = l2
-        print('finished')
+        
+        data =[]
+        curr = l1
+        while curr != None:
+            data.append(curr.val)
+            curr = curr.next
+        print(data)
+        
+        self.quickSort(data)
+        
+    def quickSort(self, data: []):
+        pivot = data[0]
+        i = 1
+        j = len(data) - 1
+        left = None
+        right = None
+        while True:
+            
+            if left is None:
+                if data[i] < pivot:
+                    i += 1
+                else:
+                    left = data[i]
+                    
+            if right is None:
+                if data[j] > pivot:
+                    j -= 1
+                else:
+                    right = data[j]
+                    
+            if left is not None and right is not None:
+                data[i] = right
+                data[j] = left
+                print(data)
+                i = 1
+                j = j = len(data) - 1
+                left = None
+                right = None
 
+            if i >= j:
+                data[0] = right
+                data[j] = pivot
+                print(data)
+                break
+        
 l1 = ListNode(3)
 l1.next = ListNode(2)
 l1.next.next = ListNode(1)
