f0054d40919c98cb1604b3d4aebfdbac307a383a
郭俊廷 <>
2019-11-12 22:13:42 +0800

    add create function of binary tree and finish 101. Symmetric Tree

--- Algorithms/Easy/101_Symmetric_Tree.py
+++ Algorithms/Easy/101_Symmetric_Tree.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).parent.parent.absolute()))
-from models.tree_node import TreeNode
+from models.binary_tree import TreeNode, arr_to_node
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        print(node.val)
-        print('ok')
+        return self.is_mirror(root, root)
     
+    def is_mirror(self, t1:TreeNode, t2:TreeNode) -> bool:
+        if t1 == None and t2 == None:
+            return True
+        if t1 == None or t2 == None:
+            return False
+        return t1.val == t2.val and self.is_mirror(t1.right,t2.left) and self.is_mirror(t1.left,t2.right)
 
-
-node = TreeNode(1)
 s = Solution()
-s.isSymmetric(node)
+node = None
+arr = [1,2,2,3,4,4,3]
+node = arr_to_node(arr,node, 0, len(arr))
+print(node.level_order(node))
+answer = s.isSymmetric(node)
+print(answer)
---
+++ Algorithms/models/binary_tree.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+    def in_order(self):
+        if self.left:
+            self.left.in_order()
+        print(self.val)
+        if self.right:
+            self.right.in_order()
+            
+    def level_order(self,root):
+        res = []
+        self.dfs(root, 0, res)
+        return res
+            
+    def dfs(self, root, depth, res):
+        if root == None:
+            return res
+        if len(res) < depth+1:
+            res.append([])
+        res[depth].append(root.val)
+        self.dfs(root.left, depth+1, res)
+        self.dfs(root.right, depth+1, res)
+        
+def arr_to_node(arr, root, i, n): 
+    if i < n: 
+     
+        root = TreeNode(arr[i])  
+        root.left = arr_to_node(arr, root.left, 2 * i + 1, n)  
+        root.right = arr_to_node(arr, root.right, 2 * i + 2, n) 
+        
+    return root 
+
+# arr = [1, 2, 3, 4, 5, 6, 6, 6, 6] 
+# n = len(arr) 
+# root = None
+# root = array_to_tree_node(arr, root, 0, n) 
+# print('in order')
+# root.in_order()
+# print('level order')
+# root.level_order(root)
