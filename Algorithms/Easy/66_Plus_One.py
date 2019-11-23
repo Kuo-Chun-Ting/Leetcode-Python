8ded554a9eabe884238b02782d36507f80624c4b
郭俊廷 <>
2019-11-23 15:36:49 +0800

    rename

--- Algorithms/Easy/66_Plus_One.py
+++ Algorithms/Easy/66_Plus_One.py
@@ -2,31 +2,11 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
-        length = len(digits)
-        carry = 0
-        sum = digits[length - 1] + 1
-        
-        if sum >= 10:
-            digits[length - 1] = sum - 10
-            carry = 1
-        else:
-            digits[length - 1] = digits[length - 1] + 1
-            return digits
-        
-        for i in range(length - 2, -1, -1):
-            if carry + digits[i] >= 10:
-                digits[i] = digits[i] + carry - 10
-                carry = 1
-            else:
-                digits[i] = digits[i] + 1
-                carry = 0
-                return digits
-            
-        if carry != 0:
-            digits.insert(0,1)
-            
-        return digits
+        toStr = ''.join(str(i) for i in digits)
+        toInt = int(toStr)
+        toInt += 1
+        result = [int(i) for i in str(toInt)]
+        return result
         
 s = Solution()
-print(s.plusOne([9]))
+print(s.plusOne([9,2,3,4]))
