7d9a9566433b03995a3fd6bd2149ca9579e14e16
郭俊廷 <>
2019-11-23 12:24:19 +0800

    clean code

--- Algorithms/Easy/66_Plus_One.py
+++ Algorithms/Easy/66_Plus_One.py
@@ -2,25 +2,27 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        
         length = len(digits)
-        cash = 0
+        carry = 0
         sum = digits[length - 1] + 1
+        
         if sum >= 10:
             digits[length - 1] = sum - 10
-            cash = 1
+            carry = 1
         else:
             digits[length - 1] = digits[length - 1] + 1
             return digits
         
         for i in range(length - 2, -1, -1):
-            if cash + digits[i] >= 10:
-                digits[i] = digits[i] + cash -10
-                cash = 1
+            if carry + digits[i] >= 10:
+                digits[i] = digits[i] + carry - 10
+                carry = 1
             else:
                 digits[i] = digits[i] + 1
-                cash = 0
+                carry = 0
                 return digits
-        if cash != 0:
+        if carry != 0:
             digits.insert(0,1)
         return digits
         
