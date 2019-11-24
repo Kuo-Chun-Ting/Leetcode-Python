5af9ef17e140d569aa7e3816ef1a02c9060573de
郭俊廷 <>
2019-11-25 00:38:40 +0800

    need to debug case [0],0,[1],1

--- Algorithms/Easy/88_Merge_Sorted_Array.py
+++ Algorithms/Easy/88_Merge_Sorted_Array.py
@@ -2,23 +2,17 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(len(nums1)-m):
-            nums1.remove(0)
-        for i in range(len(nums2)-n):
-            nums2.remove(0)
-            
         position = 0
         for i in range(len(nums2)):
             for j in range(position, len(nums1)):
                 if nums2[i] >= nums1[j] and j < len(nums1)-1 and nums1[j+1] >= nums2[i] or nums1[j+1] == 0:
                     nums1.insert(j+1,nums2[i])
-                    position = j+2
+                    position = j+1
                     break
-        
-        
-            
+        for i in range(len(nums1)-m-n):
+            nums1.remove(0)
 s = Solution()
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
-s.merge(nums1,3,nums2,3)
+nums1 = [0]
+nums2 = [1]
+s.merge(nums1,0,nums2,1)
 print(nums1)
