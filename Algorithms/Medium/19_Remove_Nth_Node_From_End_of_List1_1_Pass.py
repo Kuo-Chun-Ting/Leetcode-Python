1e75adc9a4cca40c64bc010d8f92783bf4653c18
郭俊廷 <>
2019-12-08 00:47:39 +0800

    modify gap variable increasing from 1

--- Algorithms/Medium/19_Remove_Nth_Node_From_End_of_List1_1_Pass.py
+++ Algorithms/Medium/19_Remove_Nth_Node_From_End_of_List1_1_Pass.py
@@ -14,7 +14,7 @@ class Solution:
         first_last = None
         second_last = None
         
-        gap = 0
+        gap = 1
         while first.next != None:
             first_last = first
             first = first.next
@@ -24,32 +24,31 @@ class Solution:
             if gap < n:
                 gap += 1
                 
-        if n == 0:
-            return
-        elif n <= gap:
-            self.remove_node(head, second, second.next)    
-        else:
+        if gap == n:
+            # normal confition
+            if second_last is None: # curr is at the beginning
+                head = second.next
+                second.next = None
+                second = None
+            elif second.next is None: # Purr is at the end
+                second_last.next = None
+            else:
+                second_last.next = second.next # curr is at the end
+                second.next = None
+        elif gap < n:
+            # n is out of the length of ListNode
             head = second.next
             second.next = None
             second = None
+        else:
+            # n is less then 0
+            return
         return head
-    
-    # remove node
-    def remove_node(self, head, last, curr):
-            if last is None: # curr is at the beginning
-                head = curr.next
-                curr.next = None
-                curr = None
-            elif curr.next is None: # curr is at the end
-                last.next = None
-            else:
-                last.next = curr.next
-                curr.next = None
             
 s = Solution()
 node = array_to_list_node([0,1,2,3,4,5,6])
 print('before')
 travel_node(node)
-node = s.removeNthFromEnd(node, 1)
+node = s.removeNthFromEnd(node, -2)
 print('after')
 travel_node(node)
