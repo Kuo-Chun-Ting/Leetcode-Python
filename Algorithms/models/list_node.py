a1ee8aaead1a5f52475ef7ed1e0860200abe5af0
郭俊廷 <>
2019-12-09 00:48:57 +0800

    add print_all and arr_to_node methods

--- Algorithms/models/list_node.py
+++ Algorithms/models/list_node.py
@@ -1,4 +1,24 @@
 class ListNode:
     def __init__(self, x):
         self.val = x
-        self.next = None
+        self.next = None       
+    
+    def print_all(self):
+        curr = self
+        while curr != None:
+            print(curr.val)
+            curr = curr.next
+            
+def arr_to_node(arr:[]) -> ListNode:
+    if arr is None or len(arr) == 0:
+        return None
+    
+    head = ListNode(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        curr.next = ListNode(arr[i])
+        curr = curr.next
+    return head
+
+node = arr_to_node([1,2,3])
+node.print_all()
