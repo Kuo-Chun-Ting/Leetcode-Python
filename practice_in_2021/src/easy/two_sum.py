1f9677f69fa34e1a0fffb5302e6803432f87fd2f
Lillard Kuo <lillard.kuo@lileesystems.com>
2021-08-10 00:36:28 +0800

    Finish the twoSum.

--- practice_in_2021/src/easy/two_sum.py
+++ practice_in_2021/src/easy/two_sum.py
@@ -2,4 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+
+        for i in range(0, len(nums), 1):
+            for j in range(len(nums)-1, i, -1):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                
+                if j-1 == i:
+                    break
+        return None
--- practice_in_2021/src/tests/easy/test_two_sum.py
+++ practice_in_2021/src/tests/easy/test_two_sum.py
@@ -1,8 +1,13 @@
+import pytest
+
 from easy.two_sum import Solution
 
-# @pytest.mark.parametrize("nums,target", [[2,7,11,15], 9])
-def test():
-    # Arrange
+@pytest.mark.parametrize("nums,target,expected", [
+    ([2,7,11,15], 9, [0,1]), ([3,2,2,3], 6, [0,3]), ([2,1,1,2], 2, [1,2])
+])
+def test_twoSum(nums, target, expected):
     # Act
+    result = Solution().twoSum(nums, target)
+
     # Assert
-    pass
+    assert result == expected
